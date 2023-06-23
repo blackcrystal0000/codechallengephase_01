e54ded579ad1ee92e3cf36fa80cb1e403ddeb949
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-06-23 11:37:59 -0700

    Update speedDetector.js

--- speedDetector.js
+++ speedDetector.js
@@ -1,16 +1,26 @@
-function scanSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerTwoPoints = 5;
-    const excessPoints = 12;
+function detectSpeed() {
+     let input = prompt('Enter speed');
+     speed = parseInt(input);
+     let points = 0;
      
-    if (speed <= speedLimit + kmPerTwoPoints) {
-        return "Okay";
+    if (speed <= 70) {
+        let speedLimit = "Okay"
+        console.log(speedLimit);
+        document.getElementById('speedOutput').innerHTML = `speedOutput:, ${speedLimit}`;
     }
 
-    else if (speed > speedLimit - excessPoints) {
-        return "License Suspended";
+    else if(speed > 70){
+        points = Math.floor((speed - 70) / 5) * 1;
+        console.log(`License suspended`);
+        document.getElementById('speedOutput').innerHTML = `speedOutput:, ${points}`;
     }
+
+    if (points >= 12){
+        caution = "License is Suspended";
+        console.log(caution);
+        document.getElementById('speedOutput').innerHTML = `speedOutput:, ${points}`; 
+        document.getElementById('caution').innerHTML = `Caution:, ${`Your{caution}`}`;
+    }
+
+  
 }
-
-scanSpeed(speed)
-console.log(scanSpeed(speed))
