b10262631f3ef292a80479a61a5e76e8cc452b4a
blackcrystal0000 <125825960+blackcrystal0000@users.noreply.github.com>
2023-06-23 11:41:48 -0700

    Update studentGGenerator.js

--- studentGGenerator.js
+++ studentGGenerator.js
@@ -1,23 +1,40 @@
-function totalGrade(_grade) {
-    const totalMarks = totalMarks("total student marks(from 0 - 100):");
-const totalNumbers = Number(totalMarks);
+function generateGrade() {
+    const gradeInput = document.getElementById('studentGradeInput');
+    const totalNumbers = parseInt(gradeInput.value);
+  
+    if (totalNumbers >= 0 && totalNumbers <= 100) {
+      console.log('Your input is ' + totalNumbers);
+  
+      let grade = '';
+  
+      if (totalNumbers > 79) {
+        console.log('Grade A');
+        grade = 'Grade A';
+      } else if (totalNumbers >= 60 && totalNumbers <= 79) {
+        console.log('Grade B');
+        grade = 'Grade B';
+      } else if (totalNumbers >= 49 && totalNumbers <= 59) {
+        console.log('Grade C');
+        grade = 'Grade C';
+      } else if (totalNumbers >= 40 && totalNumbers <= 49) {
+        console.log('Grade D');
+        grade = 'Grade D';
+      } else {
+        console.log('Grade E');
+        grade = 'Grade E';
+      }
+  
+      const gradeOutput = document.getElementById('gradeOutput');
+      if (gradeOutput) {
+        gradeOutput.textContent = grade;
+      }
+    } else {
+      console.log('Invalid Marks');
+    }
+  }
 
-if (totalNumbers > 79) {
-    return "A";
-}
-else if (totalNumbers >= 60 && totalNumbers <= 79){
-    return "B";
-}
-else if (totalNumbers >= 49 && totalNumbers <= 59){
-    return "C";
-}
-else if (totalNumbers >= 40 && totalNumbers <= 49){
-    return "D";
-}
-else {
-    return "E";
-}
-    
-}
-totalGrade(_grade);
-console.log(totalGrade(_grade))
+  
+
+
+
+
